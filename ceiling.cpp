382b7d5a1806ea4c3b9ef1c3d3cb20e934761078
Reyone Chaudhary <118600713+reyone22@users.noreply.github.com>
2022-12-02 18:07:21 +0545

    Update ceiling.cpp

--- ceiling.cpp
+++ ceiling.cpp
@@ -1,3 +1,7 @@
+/*Q.n.3 Write a program that takes two or more sets as input and produces set
+operations like union, intersection, difference and symmetric difference as
+its output.*/
+
 #include<iostream>
 using namespace std;
 int main(){
